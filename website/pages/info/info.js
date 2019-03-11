86087d22ea11410748d0831a5d473bff8ad0bc35
paplepel93 <47503656+paplepel93@users.noreply.github.com>
2019-03-11 21:58:16 +0100

    now uses class book objects

--- website/pages/info/info.js
+++ website/pages/info/info.js
@@ -2,70 +2,103 @@ function insertAllContent() {
     styleBody(document.getElementById("body"));
     addNav();
     styleNav(document.getElementById("nav1"));
-    let theFellowshipOfTheRing = new LordOfTheRings("The Fellowship of the Ring", "J.R.R. Tolkien", "Literature & Fiction", "The Saul Zaentz Company", "432", "978-0547928210", "4,7/5", "Lord of the Rings");
-    let theTwoTowers = new LordOfTheRings("The Two Towers", "J.R.R. Tolkien", "Literature & Fiction", "The Saul Zaentz Company", "352", "978-0547928203", "4,9/5", "Lord of the Rings");
-    let theReturnOfTheKing = new LordOfTheRings("The Return of the King", "J.R.R. Tolkien", "Literature & Fiction", "The Saul Zaentz Company", "432", "978-0547928197", "4,8/5", "Lord of the Rings");
-    /*addTextElement("title", "titletext", "Lord of the Rings: The Fellowship of the Ring"); //de titel node vertakt in 3 verschillende bomen (1 voor elk boek)
-    addTextElement("titletext", "authortext", "Author: J.R.R. Tolkien");                    // elk boek heeft zelf een lineare boom die steeds van 1 node vertakt naar 1 andere node
-    addTextElement("authortext", "genretext", "Genre: Literature & Fiction");
-    addTextElement("genretext", "publishertext", "Publisher: The Saul Zaentz Company");
-    addTextElement("publishertext", "pagestext", "Number of pages: 432");
-    addTextElement("pagestext", "ISBMtext", "ISBM: 978-0547928210");
-    addTextElement("ISBMtext", "ratingtext", "Average Amazon rating: 4,7/5");
-    addIMGElement("ratingtext", "img1", "../../img/slider1.jpg"); //probeer er een plaatje in te krijgen
+    let FOTR = new Book("The Fellowship of the Ring", "J.R.R. Tolkien", "Literature & Fiction", "The Saul Zaentz Company", "432", "978-0547928210", "4,7/5", "Lord of the Rings");
+    let TTT = new Book("The Two Towers", "J.R.R. Tolkien", "Literature & Fiction", "The Saul Zaentz Company", "352", "978-0547928203", "4,9/5", "Lord of the Rings");
+    let ROTK = new Book("The Return of the King", "J.R.R. Tolkien", "Literature & Fiction", "The Saul Zaentz Company", "432", "978-0547928197", "4,8/5", "Lord of the Rings");
+    ///////////////////////////////////////////////////////////////////////////////////
+    addTextElement("title", "titletext", FOTR.name); //de titel node vertakt in 3 verschillende bomen (1 voor elk boek)
+    addTextElement("titletext", "authortext", FOTR.author);                    // elk boek heeft zelf een lineare boom die steeds van 1 node vertakt naar 1 andere node
+    addTextElement("authortext", "genretext", FOTR.genre);
+    addTextElement("genretext", "publishertext", FOTR.publisher);
+    addTextElement("publishertext", "pagestext", FOTR.pages);
+    addTextElement("pagestext", "ISBMtext", FOTR.ISBN);
+    addTextElement("ISBMtext", "ratingtext", FOTR.rating);
+    addTextElement("ratingtext", "serietext", FOTR.series);
+    addIMGElement("serietext", "img1", "../../img/slider1.jpg");
     ////////////////////////////////////////////////////////////////////////////////////////
-    addTextElement("title", "title2text", "Lord of the Rings: The Two Towers");
-    addTextElement("title2text", "author2text", "Author: J.R.R. Tolkien");
-    addTextElement("author2text", "genre2text", "Genre: Literature & Fiction");
-    addTextElement("genre2text", "publisher2text", "Publisher: The Saul Zaentz Company");
-    addTextElement("publisher2text", "pages2text", "Number of pages: 352");
-    addTextElement("pages2text", "ISBM2text", "ISBM: 978-0547928203");
-    addTextElement("ISBM2text", "rating2text", "Average Amazon rating: 4,9/5");
+    addTextElement("title", "title2text", TTT.name);
+    addTextElement("title2text", "author2text", TTT.author);
+    addTextElement("author2text", "genre2text", TTT.genre);
+    addTextElement("genre2text", "publisher2text", TTT.publisher);
+    addTextElement("publisher2text", "pages2text", TTT.pages);
+    addTextElement("pages2text", "ISBM2text", TTT.ISBN);
+    addTextElement("ISBM2text", "rating2text", TTT.rating);
+    addTextElement("rating2text", "serie2text", TTT.series);
+    addIMGElement("serie2text", "img2", "../../img/slider2.jpg");
     /////////////////////////////////////////////////////////////////////////////////////
-    addTextElement("title", "title3text", "Lord of the Rings: The Return of the King");
-    addTextElement("title3text", "author3text", "Author: J.R.R. Tolkien");
-    addTextElement("author3text", "genre3text", "Genre: Literature & Fiction");
-    addTextElement("genre3text", "publisher3text", "Publisher: The Saul Zaentz Company");
-    addTextElement("publisher3text", "pages3text", "Number of pages: 432");
-    addTextElement("pages3text", "ISBM3text", "ISBM: 978-0547928197");
-    addTextElement("ISBM3text", "rating3text", "Average Amazon rating: 4,8/5");
+    addTextElement("title", "title3text", ROTK.name);
+    addTextElement("title3text", "author3text", ROTK.author);
+    addTextElement("author3text", "genre3text", ROTK.genre);
+    addTextElement("genre3text", "publisher3text", ROTK.publisher);
+    addTextElement("publisher3text", "pages3text", ROTK.pages);
+    addTextElement("pages3text", "ISBM3text", ROTK.ISBN);
+    addTextElement("ISBM3text", "rating3text", ROTK.rating);
+    addTextElement("rating3text", "serie3text", ROTK.series);
+    addIMGElement("serie3text", "img3", "../../img/slider3.jpg");
     ////////////////////////////////////////////////////////////////////////////////////////////
-    styleElement(document.getElementById("titletext"), "gold", "0%", "0px", "35px", "rgb(0,0,0,0.0)", "66%");
-    styleElement(document.getElementById("authortext"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)");
-    styleElement(document.getElementById("genretext"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)");
-    styleElement(document.getElementById("publishertext"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)");
-    styleElement(document.getElementById("pagestext"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)");
-    styleElement(document.getElementById("ISBMtext"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)");
-    styleElement(document.getElementById("ratingtext"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)");
+    styleElement(document.getElementById("titletext"), "gold", "0%", "0px", "35px", "rgb(0,0,0,0.2)", "66%");
+    styleElement(document.getElementById("authortext"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.0)");
+    styleElement(document.getElementById("genretext"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.0)");
+    styleElement(document.getElementById("publishertext"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.0)");
+    styleElement(document.getElementById("pagestext"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.0)");
+    styleElement(document.getElementById("ISBMtext"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.0)");
+    styleElement(document.getElementById("ratingtext"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.0)");
+    styleElement(document.getElementById("serietext"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.0)");
     /////////////////////////////////////////////////////////////////////////////////////////////////
-    styleElement(document.getElementById("title2text"), "gold", "33%", "0px", "35px", "rgb(0,0,0,0.0)", "33%");
-    styleElement(document.getElementById("author2text"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)", );
-    styleElement(document.getElementById("genre2text"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)",);
-    styleElement(document.getElementById("publisher2text"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)",);
-    styleElement(document.getElementById("pages2text"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)",);
-    styleElement(document.getElementById("ISBM2text"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)",);
-    styleElement(document.getElementById("rating2text"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)");
+    styleElement(document.getElementById("title2text"), "gold", "33%", "0px", "35px", "rgb(0,0,0,0.2)", "33%");
+    styleElement(document.getElementById("author2text"), "gold", "0%", "40px", "25px" );
+    styleElement(document.getElementById("genre2text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("publisher2text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("pages2text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("ISBM2text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("rating2text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("serie2text"), "gold", "0%", "40px", "25px" );
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    styleElement(document.getElementById("title3text"), "gold", "66%", "0px", "35px", "rgb(0,0,0,0.0)", "0%");
-    styleElement(document.getElementById("author3text"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)", );
-    styleElement(document.getElementById("genre3text"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)",);
-    styleElement(document.getElementById("publisher3text"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)",);
-    styleElement(document.getElementById("pages3text"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)",);
-    styleElement(document.getElementById("ISBM3text"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)",);
-    styleElement(document.getElementById("rating3text"), "gold", "0%", "40px", "25px", "rgb(0,0,0,0.2)");*/
+    styleElement(document.getElementById("title3text"), "gold", "66%", "0px", "35px", "rgb(0,0,0,0.2)", "0%");
+    styleElement(document.getElementById("author3text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("genre3text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("publisher3text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("pages3text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("ISBM3text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("serie3text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("rating3text"), "gold", "0%", "40px", "25px");
     //////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 }
 
+/*class Book {
+
+    constructor(name, author, pages){
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+    }
+    talk()
+    {
+        window.alert("weyo");
+    }
+}
+
+class LordOfTheRings extends Book{
+    constructor(name, author, pages){
+        super(name);
+        super(author);
+        super(pages);
+    }
+}
+*/
+
 class Book {
-    constructor(name, author, genre, publisher, pages, ISBN, rating){
+    constructor(name, author, genre, publisher, pages, ISBN, rating, series){
         this.name = name;
         this.author = author;
         this.genre = genre;
         this.publisher = publisher;
-        this.pages = pages;
         this.ISBN = ISBN;
         this.rating = rating;
+        this.pages = pages;
+        this.series = series;
     }
 }
 
@@ -78,9 +111,10 @@ class LordOfTheRings extends Book{
         super(pages);
         super(ISBN);
         super(rating);
-        this.series = series;
+        this._series = series;
     }
 }
+
 
 function addNav () {
     var names = ["Home", "Info", "Summary", "Characters", "Author", "Middle-Earth"];
@@ -96,13 +130,14 @@ function addNav () {
     }
 }
 
-function addIMGElement(id, newid, source) {                                //dit is mijn pging tot het bouwen van een functie die een imgnode maakt
-    var newDiv = document.createElement("div");                   // gaat alleen niet helemaal goed, maar weet niet wat er fout gaat helaas
+function addIMGElement(id, newid, source) {
+    var newDiv = document.createElement("div");
     newDiv.id = newid;
     var newContent = document.createElement("img");
     newContent.src = source;
     newContent.setAttribute("height", "100%");
-    newContent.setAttribute("width", "100%");
+    newContent.setAttribute("width", "70%");
+    newContent.setAttribute("marginLeft", "20%");
     newContent.setAttribute("alt", "this image is not loaded correctly")
     newDiv.appendChild(newContent);
     document.getElementById(id).appendChild(newDiv);
