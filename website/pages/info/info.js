39d13b3c63c578fbfee9c27726a6c0b2ff6bf420
ehhoogen <47507755+ehhoogen@users.noreply.github.com>
2019-03-12 17:23:50 +0100

    Revert "Merge branch 'master' of https://github.com/ehhoogen/LOTRBOOK"
    This reverts commit ac32f59909bafe01f8a2867ccf85131edd78471e, reversing
    changes made to 272c16547782d7afff47cbbaeabc61bdfa47afee.

--- website/pages/info/info.js
+++ website/pages/info/info.js
@@ -2,88 +2,95 @@ function insertAllContent() {
     styleBody(document.getElementById("body"));
     addNav();
     styleNav(document.getElementById("nav1"));
-    styleSection(document.getElementById("section"), "25vw", "40px");
-    styleSection(document.getElementById("section2"), "25vw", "40px");
-    styleSection(document.getElementById("section3"), "25vw", "40px");
-    styleDiv(document.getElementById("mainDiv"));
-  //  let tolkien = new Author("J.R.R. Tolkien");
-  //  let theFellowshipOfTheRing = new LordOfTheRings("The Fellowship of the Ring", tolkien.name, "Literature & Fiction", "The Saul Zaentz Company", "432", "978-0547928210", "4,7/5", "Lord of the Rings");
-   // let theTwoTowers = new LordOfTheRings("The Two Towers", tolkien.name, "Literature & Fiction", "The Saul Zaentz Company", "352", "978-0547928203", "4,9/5", "Lord of the Rings");
-   // let theReturnOfTheKing = new LordOfTheRings("The Return of the King", tolkien.name, "Literature & Fiction", "The Saul Zaentz Company", "432", "978-0547928197", "4,8/5", "Lord of the Rings");
+    /*let tolkien = new Author("J.R.R. Tolkien","03-01-1892");
+    let theFellowshipOfTheRing = new LordOfTheRings("The Fellowship of the Ring", tolkien.name, "Literature & Fiction", "The Saul Zaentz Company", "432", "978-0547928210", "4,7/5", "Lord of the Rings");
+    let theTwoTowers = new LordOfTheRings("The Two Towers", tolkien.name, "Literature & Fiction", "The Saul Zaentz Company", "352", "978-0547928203", "4,9/5", "Lord of the Rings");
+    let theReturnOfTheKing = new LordOfTheRings("The Return of the King", tolkien.name, "Literature & Fiction", "The Saul Zaentz Company", "432", "978-0547928197", "4,8/5", "Lord of the Rings");
     let FOTR = new Book("The Fellowship of the Ring", "J.R.R. Tolkien", "Literature & Fiction", "The Saul Zaentz Company", "432", "978-0547928210", "4,7/5", "Lord of the Rings");
     let TTT = new Book("The Two Towers", "J.R.R. Tolkien", "Literature & Fiction", "The Saul Zaentz Company", "352", "978-0547928203", "4,9/5", "Lord of the Rings");
-    let ROTK = new Book("The Return of the King", "J.R.R. Tolkien", "Literature & Fiction", "The Saul Zaentz Company", "432", "978-0547928197", "4,8/5", "Lord of the Rings");
-    let Publish = new Publisher("George Allen & Unwin", "1871", "British", "George Allen & Sons", "1917", "Australian", "Lord of the Rings");
-    let Tolkien = new Author("J.R.R. Tolkien", "South-Africa", "England", "Died in 1972 at the age of 81", "The Hobbit");
+    let ROTK = new Book("The Return of the King", "J.R.R. Tolkien", "Literature & Fiction", "The Saul Zaentz Company", "432", "978-0547928197", "4,8/5", "Lord of the Rings");*/
     ///////////////////////////////////////////////////////////////////////////////////
-    addTextElement("title", "titletext", "Name: " + FOTR.name); //de titel node vertakt in 3 verschillende bomen (1 voor elk boek)
-    addTextElement("titletext", "authortext", "Author: " + FOTR.author);                    // elk boek heeft zelf een lineare boom die steeds van 1 node vertakt naar 1 andere node
-    addTextElement("authortext", "genretext", "Genre: " + FOTR.genre);
-    addTextElement("genretext", "publishertext", "Publisher: " + FOTR.publisher);
-    addTextElement("publishertext", "pagestext", "Number of pages: " + FOTR.pages);
-    addTextElement("pagestext", "ISBNtext", "ISBN: " + FOTR.ISBN);
-    addTextElement("ISBNtext", "ratingtext", "Average amazon rating: " + FOTR.rating);
-    addTextElement("ratingtext", "serietext", FOTR.series);
-    addIMGElement("ratingtext", "img1", "../../img/slider1.jpg", "Cover of the first book of the series: the Fellowship of Ring"); //probeer er een plaatje in te krijgen
+    /*addTextElement("title", "titletext", "Lord of the Rings: The Fellowship of the Ring"); //de titel node vertakt in 3 verschillende bomen (1 voor elk boek)
+    addTextElement("titletext", "authortext", "Author: J.R.R. Tolkien");                    // elk boek heeft zelf een lineare boom die steeds van 1 node vertakt naar 1 andere node
+    addTextElement("authortext", "genretext", "Genre: Literature & Fiction");
+    addTextElement("genretext", "publishertext", "Publisher: The Saul Zaentz Company");
+    addTextElement("publishertext", "pagestext", "Number of pages: 432");
+    addTextElement("pagestext", "ISBMtext", "ISBM: 978-0547928210");
+    addTextElement("ISBMtext", "ratingtext", "Average Amazon rating: 4,7/5");
+    addIMGElement("ratingtext", "img1", "../../img/slider1.jpg"); //probeer er een plaatje in te krijgen
     ////////////////////////////////////////////////////////////////////////////////////////
-    addTextElement("img1", "astart", "Author:");
-    addTextElement("astart", "a0", "");
-    addLinkElement("a0", "a01", Tolkien.name,"https://en.wikipedia.org/wiki/J._R._R._Tolkien");
-    addTextElement("a0", "a1", "Born in " + Tolkien.bornnationality);
-    addTextElement("a1","a2", "Lived in " + Tolkien.truenationality);
-    addTextElement("a2", "a3", Tolkien.age);
-    addTextElement("a3", "a4", "Most important work: " + Tolkien.mostimportantwork);
-    ///////////////////////////////////////////////////////////////////////////////////////
-    addTextElement("title2", "title2text", "Name: " + TTT.name);
-    addTextElement("title2text", "author2text", "Author: " + TTT.author);
-    addTextElement("author2text", "genre2text", "Genre: " + TTT.genre);
-    addTextElement("genre2text", "publisher2text", "Publisher: " + TTT.publisher);
-    addTextElement("publisher2text", "pages2text", "Number of pages: " + TTT.pages);
-    addTextElement("pages2text", "ISBN2text", "ISBN: " + TTT.ISBN);
-    addTextElement("ISBN2text", "rating2text", "Average amazon rating: " + TTT.rating);
+    addTextElement("title", "title2text", TTT.name);
+    addTextElement("title2text", "author2text", TTT.author);
+    addTextElement("author2text", "genre2text", TTT.genre);
+    addTextElement("genre2text", "publisher2text", TTT.publisher);
+    addTextElement("publisher2text", "pages2text", TTT.pages);
+    addTextElement("pages2text", "ISBM2text", TTT.ISBN);
+    addTextElement("ISBM2text", "rating2text", TTT.rating);
     addTextElement("rating2text", "serie2text", TTT.series);
-    addIMGElement("rating2text", "img2", "../../img/slider2.jpg", "Cover of the second book of the series: the Two Towers");
-    ///////////////////////////////////////////////////////////////////////////////////////
-    addTextElement("img2", "pstart", "Publisher:");
-    addTextElement("pstart", "p0", "");
-    addLinkElement("p0", "p01", Publish.name,"https://en.wikipedia.org/wiki/Allen_%26_Unwin");
-    addTextElement("p0", "p1", "Founded in: " + Publish.age);
-    addTextElement("p1","p2", "Founded as: " + Publish.foundedname);
-    addTextElement("p2", "p3", "Original nationality: " + Publish.foundednationality);
-    addTextElement("p3", "p4", "Bought up in: " + Publish.merged);
-    addTextElement("p4", "p5", "New nationality: " + Publish.newnationality);
-    addTextElement("p5", "p6", "Most important work: " + Publish.mostimportantwork);
+    addIMGElement("serie2text", "img2", "../../img/slider2.jpg");
     /////////////////////////////////////////////////////////////////////////////////////
-    addTextElement("title3", "title3text", "Name: " + ROTK.name);
-    addTextElement("title3text", "author3text", "Author: " + ROTK.author);
-    addTextElement("author3text", "genre3text", "Genre: " + ROTK.genre);
-    addTextElement("genre3text", "publisher3text", "Publisher: " + Publish.name);//+ ROTK.publisher);
-    addTextElement("publisher3text", "pages3text", "Number of pages: " + ROTK.pages);
-    addTextElement("pages3text", "ISBN3text", "ISBN: " + ROTK.ISBN);
-    addTextElement("ISBN3text", "rating3text", "Average amazon rating: " + ROTK.rating);
+    addTextElement("title", "title3text", ROTK.name);
+    addTextElement("title3text", "author3text", ROTK.author);
+    addTextElement("author3text", "genre3text", ROTK.genre);
+    addTextElement("genre3text", "publisher3text", ROTK.publisher);
+    addTextElement("publisher3text", "pages3text", ROTK.pages);
+    addTextElement("pages3text", "ISBM3text", ROTK.ISBN);
+    addTextElement("ISBM3text", "rating3text", ROTK.rating);
     addTextElement("rating3text", "serie3text", ROTK.series);
-    addIMGElement("rating3text", "img3", "../../img/slider3.jpg", "Cover of the third and final book of the series: the Return of the King");
+    addIMGElement("serie3text", "img3", "../../img/slider3.jpg");
     ////////////////////////////////////////////////////////////////////////////////////////////
-    styleElement(document.getElementById("titletext"), "gold", "0%", "150%", "200%");
-    styleElement(document.getElementById("authortext"), "gold", "0%", "40px", "75%");
+    styleElement(document.getElementById("titletext"), "gold", "0%", "0px", "35px", "66%");
+    styleElement(document.getElementById("authortext"), "gold", "0%", "40px", "25px", "30px");
+    styleElement(document.getElementById("genretext"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("publishertext"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("pagestext"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("ISBMtext"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("ratingtext"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("serietext"), "gold", "0%", "40px", "25px");
     /////////////////////////////////////////////////////////////////////////////////////////////////
-    styleElement(document.getElementById("astart"), "gold", "0%", "20%", "200%");
-    styleElement(document.getElementById("a0"), "gold", "0%", "0%", "50%");
-    styleElement(document.getElementById("a01"), "gold");
+    styleElement(document.getElementById("title2text"), "gold", "33%", "0px", "35px",  "33%");
+    styleElement(document.getElementById("author2text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("genre2text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("publisher2text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("pages2text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("ISBM2text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("rating2text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("serie2text"), "gold", "0%", "40px", "25px" );
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    styleElement(document.getElementById("title2text"), "gold", "0%", "150%", "200%");
-    styleElement(document.getElementById("author2text"), "gold", "0%", "40px", "75%");
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    styleElement(document.getElementById("pstart"), "gold", "0%", "32%", "200%");
-    styleElement(document.getElementById("p0"), "gold", "0%", "0%", "50%");
-    styleElement(document.getElementById("p01"), "gold");
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    styleElement(document.getElementById("title3text"), "gold", "0%", "150%", "200%");
-    styleElement(document.getElementById("author3text"), "gold", "0%", "40px", "75%");
+    styleElement(document.getElementById("title3text"), "gold", "66%", "0px", "35px", "0%");
+    styleElement(document.getElementById("author3text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("genre3text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("publisher3text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("pages3text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("ISBM3text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("serie3text"), "gold", "0%", "40px", "25px");
+    styleElement(document.getElementById("rating3text"), "gold", "0%", "40px", "25px");
     //////////////////////////////////////////////////////////////////////////////////////////////////
 
-    document.getElementById("p8").addEventListener("click", function(){ window.alert});
+*/
 }
+
+/*class Book {
+
+    constructor(name, author, pages){
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+    }
+    talk()
+    {
+        window.alert("weyo");
+    }
+}
+
+class LordOfTheRings extends Book{
+    constructor(name, author, pages){
+        super(name);
+        super(author);
+        super(pages);
+    }
+}
+*/
 
 class Book {
     constructor(name, author, genre, publisher, pages, ISBN, rating, series){
@@ -100,39 +107,27 @@ class Book {
 
 class LordOfTheRings extends Book{
     constructor(name, author, genre, publisher, pages, ISBN, rating, series){
-        super(name, author, genre, publisher, pages, ISBN, rating);
+        super(name);
+        super(author);
+        super(genre);
+        super(publisher);
+        super(pages);
+        super(ISBN);
+        super(rating);
         this.series = series;
     }
 }
 
 class Author {
-    constructor(name, bornnat, truenat, age, miw){
+    constructor(name, birthday){
         this.name = name;
-        this.bornnationality = bornnat;
-        this.truenationality = truenat;
-        this.age = age;
-        this.mostimportantwork = miw;
-        this.link = "https://en.wikipedia.org/wiki/J._R._R._Tolkien"
-    }
-}
-
-class Publisher {
-    constructor(name, age, fnat, fname, merged, newnat, miw){
-        this.name = name;
-        this.age = age;
-        this.foundednationality = fnat;
-        this.foundedname = fname;
-        this.merged = merged;
-        this.newnationality = newnat;
-        this.mostimportantwork = miw;
-        this.link = "https://en.wikipedia.org/wiki/Allen_%26_Unwin";
-
+        this.birthday = birthday;
     }
 }
 
 function addNav () {
     var names = ["Home", "Info", "Summary", "Characters", "Author", "Middle-Earth"];
-    var links = ["../../index.html", "#", "../../summary.html", "../../characters.html", "../../author.html", "../../map.html"];
+    var links = ["../../home.html", "#", "../../summary.html", "../../characters.html", "../../author.html", "../../map.html"];
     for(let i = names.length; i > 0; i--) {
         var newContent = document.createTextNode(names[i-1]);
         var newLink = document.createElement("a");
@@ -142,7 +137,7 @@ function addNav () {
     }
 }
 
-function addIMGElement(id, newid, source, alt) {
+function addIMGElement(id, newid, source) {
     var newDiv = document.createElement("div");
     newDiv.id = newid;
     var newContent = document.createElement("img");
@@ -150,34 +145,22 @@ function addIMGElement(id, newid, source, alt) {
     newContent.setAttribute("height", "100%");
     newContent.setAttribute("width", "70%");
     newContent.setAttribute("marginLeft", "20%");
-    newContent.setAttribute("alt", alt);
+    newContent.setAttribute("alt", "this image is not loaded correctly")
     newDiv.appendChild(newContent);
     document.getElementById(id).appendChild(newDiv);
 }
 
 function addTextElement (id, newid, text) {
     // create a new div element
-    var newDiv1 = document.createElement("p");
+    var newDiv = document.createElement("div");
     // give the new div an id
-    newDiv1.id = newid;
+    newDiv.id = newid;
     // and give it some content
     var newContent = document.createTextNode(text);
     // add the text node to the newly created div
-    newDiv1.appendChild(newContent);
-    //
-    document.getElementById(id).appendChild(newDiv1);
-}
-
-function addLinkElement (id, newid, text, link) {
-    var newDiv = document.createElement('a');
-    newDiv.id = newid;
-    newDiv.href = link;
-    newDiv.title = text;
-    newDiv.target = "_blank";
-    var newContent = document.createTextNode(text);
     newDiv.appendChild(newContent);
+    //
     document.getElementById(id).appendChild(newDiv);
-
 
 }
 
@@ -197,31 +180,16 @@ function styleNav(nav){
     nav.style.height = "7.1vh";
     nav.style.backgroundColor = "rgba(160, 160, 160, 0.5)";
     nav.style.float = "left";
-    nav.style.marginTop = "0";
-    nav.style.zIndex = "100";
     for(let i = 1; i <= nav.childElementCount; i++){
-        if(i == 1){
-            nav.childNodes[i].style.float = "right";
-            nav.childNodes[i].style.textAlign = "center";
-            nav.childNodes[i].style.fontSize = "16px";
-            nav.childNodes[i].style.margin = "4.7vh 8vw 0 3.15vw";
-            nav.childNodes[i].style.color = "black";
-            nav.childNodes[i].style.textDecoration = "none";
-            nav.childNodes[i].style.textTransform = "uppercase";
-            nav.childNodes[i].addEventListener("mouseover", function() { nav.childNodes[i].style.color = "white"; });
-            nav.childNodes[i].addEventListener("mouseout", function() { nav.childNodes[i].style.color = "black"; });
-        }
-        else{
-            nav.childNodes[i].style.float = "right";
-            nav.childNodes[i].style.textAlign = "center";
-            nav.childNodes[i].style.fontSize = "16px";
-            nav.childNodes[i].style.margin = "4.7vh 3vw 0 3.4vw";
-            nav.childNodes[i].style.color = "black";
-            nav.childNodes[i].style.textDecoration = "none";
-            nav.childNodes[i].style.textTransform = "uppercase";
-            nav.childNodes[i].addEventListener("mouseover", function() { nav.childNodes[i].style.color = "white"; });
-            nav.childNodes[i].addEventListener("mouseout", function() { nav.childNodes[i].style.color = "black"; });
-        }
+        nav.childNodes[i].style.float = "right";
+        nav.childNodes[i].style.textAlign = "center";
+        nav.childNodes[i].style.fontSize = "16px";
+        nav.childNodes[i].style.margin = "4.7vh 2.97vw 0 3.15vw";
+        nav.childNodes[i].style.color = "black";
+        nav.childNodes[i].style.textDecoration = "none";
+        nav.childNodes[i].style.textTransform = "uppercase";
+        nav.childNodes[i].addEventListener("mouseover", function() { nav.childNodes[i].style.color = "white"; });
+        nav.childNodes[i].addEventListener("mouseout", function() { nav.childNodes[i].style.color = "black"; });
     }
 }
 function styleElement (element, color, marginleft, margintop, fontsize, marginright) {
@@ -230,16 +198,4 @@ function styleElement (element, color, marginleft, margintop, fontsize, marginri
     element.style.marginTop = margintop;
     element.style.fontSize = fontsize;
     element.style.marginRight = marginright;
-}
-
-function styleDiv (element){
-    element.style.position = "absolute";
-    element.style.display = "flex";
-    element.style.justifyContent = "space-around";
-    element.style.zIndex = "-100";
-}
-
-function styleSection (section, width, padding) {
-    section.style.width = width;
-    section.style.padding = padding;
 }
