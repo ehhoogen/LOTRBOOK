e39defb0b351d90aa112cfa17664654dd7676118
BRose98 <b.roseboom@students.uu.nl>
2019-03-13 16:09:49 +0100

    changed stuff

--- website/Jsmenu.js
+++ website/Jsmenu.js
@@ -1,15 +1,20 @@
+var save = document.getElementById("save");
+if(save){
+    save.addEventListener("click", colsel);
+}
+
 function colsel()
 {
-    a=document.getElementById("tselect").value;
+    var a = document.getElementById("tselect").value;
     var appear = document.getElementById('cselect').value;
-    numb=parseInt(appear);
+    var numb = parseInt(appear);
     console.log(a);
-    var list=a.split(',');
+    var list = a.split(',');
     console.log(a);
-    for (var j=0;j<list.length;j++) {
+    for (var j = 0; j < list.length; j++) {
 
         if(a==="body"){
-            var all=document.getElementsByTagName("*");
+            var all = document.getElementsByTagName("*");
         }
         else{
             var all = document.getElementById(list[j]).querySelectorAll("*");
@@ -20,17 +25,9 @@ function colsel()
         }
 
         for (let i = 0; i < all.length; i++) {
-            stylo = window.getComputedStyle(all[i], null).getPropertyValue('font-size');
-            currentSize = parseInt(stylo);
+            var stylo = window.getComputedStyle(all[i], null).getPropertyValue('font-size');
+            var currentSize = parseInt(stylo);
             all[i].style.fontSize = (currentSize+(numb/10)) + 'px';
         }
-        /*if (list.length<=1){
-            var all = document.getElementById(list[0]);
-            all.style.color = appear;
-            stylo = window.getComputedStyle(all, null).getPropertyValue('font-size');
-            currentSize = parseInt(stylo);
-            all.style.fontSize = (currentSize+numb) + 'px';
-        }*/
-
     }
 }
