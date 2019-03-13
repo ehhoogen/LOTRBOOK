1678cced49509343a9309275d4b82db051603316
BRose98 <b.roseboom@students.uu.nl>
2019-03-13 20:15:55 +0100

    small comment changes

--- website/pages/info/info.js
+++ website/pages/info/info.js
@@ -362,12 +362,12 @@ function colsel() {
     var a = document.getElementById("tselect").value;
     var appear = document.getElementById('cselect').value;
     var numb = parseInt(appear);
-    console.log(a);
+    // The selected id's will be put into a list. var a is a string with id's and spaces in between them
     var list = a.split(',');
     console.log(a);
     for (var j = 0; j < list.length; j++) {
 
-        if (a === "body") {
+        if (a === "body") {//Here every element of the page gets selected and changed with the selected change
             var all = document.getElementsByTagName("*");
         } else {
             var all = document.getElementById(list[j]).querySelectorAll("*");
@@ -377,7 +377,7 @@ function colsel() {
             all[i].style.color = appear;
         }
 
-        for (let i = 0; i < all.length; i++) {
+        for (let i = 0; i < all.length; i++) {//Here the selected part of the page, in the dropdown menu, changed with the selected change
             var stylo = window.getComputedStyle(all[i], null).getPropertyValue('font-size');
             var currentSize = parseInt(stylo);
             all[i].style.fontSize = (currentSize + (numb / 10)) + 'px';
